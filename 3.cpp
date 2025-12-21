2d49f2d0b3e681732bc323771f73dece57fb1f53
dimonkrutoiprogrammist <dmitry2008cita@gmail.com>
2025-12-21 16:08:33 +0900

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
-#include <algorithm> //Подключение библиотеки для reverse
-#include <cstring> //Подключение библиотеки для reverse
+#include <algorithm> 
+#include <cstring> 
 using namespace std;
 
 void Output(const string &stroka){
 
     //Переменная для подсчета длины строки
     int n = stroka.length();  
-    string str = ""; //Строка, в которой будут находиться уникальные символы
+    string str = "";
 
-    //Цикл для удаления повторных символов
+
     for(int i = 0; i < n; i++){
         char c = stroka[i];
-        //Начальное значение до проверки false
+      
         bool found = false;
 
         //Перебор строки для проверки повтора
         for(int j = 0; j < str.length(); j++){
-            //Если строка прохода совпадает с проверочной строкой, то true
+           
             if(str[j] == c){
                 found = true;
                 break;
             }
         }
-        //Если символ не встретился, то добавляем в str
+       
         if(found == false){
             str += c;
         }
@@ -41,8 +41,8 @@ int main(){
     getline(cin, stroka);
     //Вывод исходной строки
     cout << "Input: " << stroka;
-    
-    //Применение функции и вывод готовой строки
+   
     Output(stroka);
     return 0;
+
 }
